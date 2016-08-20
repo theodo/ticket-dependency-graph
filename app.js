23d2b945a6b5509d82887736b89c1d1d855a165a
Victor Quach <victorq@theodo.fr>
2016-08-20 19:35:53 +0200

    Parse complexity from name

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ myDiagram.nodeTemplate = GO(
     { background: "#2221AD" },
     GO(go.TextBlock, { margin: 12, stroke: "yellow", font: "bold 16px sans-serif" },
     new go.Binding("text", "key")),
+    GO(go.TextBlock, { margin: 12, stroke: "#64AD35", font: "bold 14px sans-serif" },
+    new go.Binding("text", "complexity")),
     GO(go.TextBlock, { margin: 8, stroke: "white", font: "bold 10px sans-serif",  width: 100, wrap: go.TextBlock.WrapFit },
     new go.Binding("text", "name"))
 );
@@ -19,11 +21,11 @@ myDiagram.nodeTemplate = GO(
 //To be populated with Trello
 var myModel = GO(go.GraphLinksModel);
 myModel.nodeDataArray = [
-    { key: 1 , name: "ETQU, j'ai un arbre de dépendances"},
-    { key: 2 , name: "ETQU, j'ai la liste des tickets de mon board"},
-    { key: 3 , name: "ETQU, j'ai un arbre de dépendances avec les tickets de mon boards"},
-    { key: 4 , name: "ETQVictor, je réussis mon PISCAR"},
-    { key: 5 , name: "ETQDev, j'ai accès à l'API TRello"},
+    { key: 1 , complexity: 13, name: "ETQU, j'ai un arbre de dépendances"},
+    { key: 2 , complexity: 8, name: "ETQU, j'ai la liste des tickets de mon board"},
+    { key: 3 , complexity: 1, name: "ETQU, j'ai un arbre de dépendances avec les tickets de mon boards"},
+    { key: 4 , complexity: 21, name: "ETQVictor, je réussis mon PISCAR"},
+    { key: 5 , complexity: null, name: "ETQDev, j'ai accès à l'API TRello"},
 ];
 
 myModel.linkDataArray =
@@ -38,6 +40,21 @@ myDiagram.linkTemplate =
     GO(go.Shape, { strokeWidth: 3, stroke: "#555" }));
 
 myDiagram.model = myModel;
+
+
+var getComplexityFromName = function(name) {
+    if (name[0] != '(') {
+        return null;
+    }
+    return res = parseInt(name.split(")")[0].split("(")[1]);
+}
+
+var getNameWithoutComplexity = function(name) {
+    if (name[0] != '(') {
+        return name;
+    }
+    return name.split(")")[1];
+}
 
 var main = new Vue({
     el: '#graphHandler',
@@ -64,12 +81,15 @@ var main = new Vue({
             currentNode = myDiagram.model.findNodeDataForKey(ticketId)
             if (null == currentNode) {
                 myDiagram.startTransaction("Add ticket");
-                var newTicket = { key: ticketId, name: ticketName};
+                var newTicket = { key: ticketId,
+                                  name: getNameWithoutComplexity(ticketName),
+                                  complexity: getComplexityFromName(ticketName)};
                 myDiagram.model.addNodeData(newTicket);
                 myDiagram.commitTransaction("Add ticket");
             } else {
                 myDiagram.startTransaction("Update ticket");
-                myDiagram.model.setDataProperty(currentNode, "name", ticketName);
+                myDiagram.model.setDataProperty(currentNode, "name", getNameWithoutComplexity(ticketName));
+                myDiagram.model.setDataProperty(currentNode, "complexity", getComplexityFromName(ticketName));
                 myDiagram.commitTransaction("Update ticket");
             }
         },
