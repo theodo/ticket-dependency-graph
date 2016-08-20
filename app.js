11c4e4d9cfe9986c21cd294f6d4a72241cbf72da
Victor Quach <victorq@theodo.fr>
2016-08-20 13:58:06 +0200

    Graph without Trello

---
+++ app.js
@@ -0,0 +1,56 @@
+var GO = go.GraphObject.make;
+var myDiagram = GO(go.Diagram, "dependancyGraph", {
+    initialContentAlignment: go.Spot.Center,
+    "undoManager.isEnabled": true, // enable Ctrl-Z to undo and Ctrl-Y to redo
+    layout: GO(go.LayeredDigraphLayout,
+        { angle: 90, layerSpacing: 10 })
+});
+
+myDiagram.nodeTemplate = GO(
+    go.Node,
+    "Horizontal",
+    { background: "#2221AD" },
+    GO(go.TextBlock, { margin: 12, stroke: "yellow", font: "bold 16px sans-serif" },
+    new go.Binding("text", "key")),
+    GO(go.TextBlock, { margin: 8, stroke: "white", font: "bold 10px sans-serif",  width: 100, wrap: go.TextBlock.WrapFit },
+    new go.Binding("text", "name"))
+);
+
+//To be populated with Trello
+var myModel = GO(go.GraphLinksModel);
+myModel.nodeDataArray = [
+    { key: 1 , name: "ETQU, j'ai un arbre de dépendances"},
+    { key: 2 , name: "ETQU, j'ai la liste des tickets de mon board"},
+    { key: 3 , name: "ETQU, j'ai un arbre de dépendances avec les tickets de mon boards"},
+    { key: 4 , name: "ETQVictor, je réussis mon PISCAR"},
+    { key: 5 , name: "ETQDev, j'ai accès à l'API TRello"},
+];
+
+myModel.linkDataArray =
+[
+    { from: 1, to: 3 },
+    { from: 2, to: 3 },
+    { from: 3, to: 4 }
+];
+
+myDiagram.model = myModel;
+
+new Vue({
+    el: '#graphHandler',
+
+    data: {
+        currentParent: '',
+        currentChild: ''
+    },
+
+    methods: {
+        addDependancy: function() {
+            myDiagram.startTransaction("Add dependancy");
+            myDiagram.model.addLinkData({
+                from: parseInt(this.currentParent),
+                to: parseInt(this.currentChild)
+            })
+            myDiagram.commitTransaction("Add dependancy");
+        }
+    }
+})
