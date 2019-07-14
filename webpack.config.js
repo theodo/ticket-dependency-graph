0b0fd5906e1fa706f24c5876cb5324ac76f23379
ivanosevitch <ivanp@theodo.fr>
2019-07-14 16:09:02 +0200

    Update webpack to v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/main.js',
+  mode: 'development',
   output: {
-    path: __dirname,
-    filename: './dist/app.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
   },
   module: {
-    loaders: [{ test: /\.css$/, loader: 'style!css' }]
-  }
+    rules: [{ test: /\.css$/, loader: 'style!css' }],
+  },
+  devServer: {
+    contentBase: './',
+    publicPath: '/dist/',
+  },
 };
