363f0e29ba65299b4fffa83246bec6a4c79025c7
ivanosevitch <ivanp@theodo.fr>
2019-11-14 15:51:21 +0100

    Add comment about Google Analytics not working

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ require('./trelloHandler.js');
 require('./graphHandler.js');
 
 Vue.use(VueAnalytics, {
-  id: 'UA-121199171-2', // ask access to ivanp@theodo.fr
+  id: 'UA-121199171-2', // ask access to ivanp@theodo.fr. Google analytics is not working at the moment, will be fixed https://trello.com/c/gSyGL6B8
 });
