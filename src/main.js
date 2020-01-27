829b189f16dcf2c105408e0f07dbe8d97ecdbd4f
ivanosevitch <ivanp@theodo.fr>
2020-01-27 11:09:15 +0100

    Switch more "require" imports to "import"

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 
+import './trelloHandler';
+import './graphHandler';
+
 Vue.use(VueGtag, {
   config: { id: 'UA-121199171-2' },
 });
-
-require('./trelloHandler.js');
-require('./graphHandler.js');
