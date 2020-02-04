a9fa23ea6b4bcf0574c5e39c4d80f99a7d0350db
Victor Lebrun <victor.lebrun011@gmail.com>
2020-02-04 23:45:38 +0100

    Add trello colors for labels

--- src/trelloHandler.js
+++ src/trelloHandler.js
@@ -5,6 +5,19 @@ import './graphHandler';
 const lastBoardChoice = 'lastBoardChoice';
 const lastListChoice = 'lastListChoice';
 
+const trelloColors = {
+  black: '#344563',
+  blue: '#0079bf',
+  green: '#61bd4f',
+  lime: '#51e898',
+  orange: '#ff9f1a',
+  pink: '#ff78cb',
+  purple: '#c377e0',
+  red: '#eb5a46',
+  sky: '#00c2e0',
+  yellow: '#f2d600',
+};
+
 window.trelloHandler = new Vue({
   el: '#trello',
 
@@ -121,7 +134,10 @@ window.trelloHandler = new Vue({
         window.graphHandler.addOrUpdateTicket({
           ticketId: card.idShort,
           ticketName: card.name,
-          ticketLabels: card.labels,
+          ticketLabels: card.labels.map(ticketLabel => ({
+            color: trelloColors[ticketLabel.color],
+            name: ticketLabel.name,
+          })),
         });
       }
     },
