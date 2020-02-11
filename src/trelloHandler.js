c8d1370446aa5993c7be4b84badca515a0c2dbaf
ivanosevitch <ivanp@theodo.fr>
2020-02-11 08:43:44 +0100

    Make label colors come from Trello color list in example graph

--- src/trelloHandler.js
+++ src/trelloHandler.js
@@ -1,22 +1,10 @@
 import Vue from 'vue';
+import trelloColors from './trelloColors';
 import './trelloApiHelper';
 import './graphHandler';
 
 const lastBoardChoice = 'lastBoardChoice';
 const lastListChoice = 'lastListChoice';
-
-const trelloColors = {
-  black: '#344563',
-  blue: '#0079bf',
-  green: '#61bd4f',
-  lime: '#51e898',
-  orange: '#ff9f1a',
-  pink: '#ff78cb',
-  purple: '#c377e0',
-  red: '#eb5a46',
-  sky: '#00c2e0',
-  yellow: '#f2d600',
-};
 
 window.trelloHandler = new Vue({
   el: '#trello',
